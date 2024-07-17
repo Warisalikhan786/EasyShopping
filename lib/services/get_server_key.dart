6b102dd329dd58eaa5df37d977d9dfa5730d795b
Waris Software House Jhelum <warissoftwarehousejhelum@gmail.com>
2024-07-17 16:16:52 +0500

    Get Server Token

---
+++ lib/services/get_server_key.dart
@@ -0,0 +1,18 @@
+import 'package:googleapis_auth/auth_io.dart';
+
+class GetServerKey {
+  Future<String> getServerKeyToken() async {
+    final scopes = [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/firebase.database',
+      'https://www.googleapis.com/auth/firebase.messaging',
+    ];
+
+    final client = await clientViaServiceAccount(
+      ServiceAccountCredentials.fromJson('Your server details'),
+      scopes,
+    );
+    final accessServerKey = client.credentials.accessToken.data;
+    return accessServerKey;
+  }
+}
