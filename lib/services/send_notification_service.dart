db0d4441e8409dbf145b009202628d3d074b2af7
Waris Software House Jhelum <warissoftwarehousejhelum@gmail.com>
2024-08-25 15:44:49 +0500

    Send firebase push notification using api

---
+++ lib/services/send_notification_service.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'get_server_key.dart';
+import 'package:http/http.dart' as http;
+
+class SendNotificationService {
+  static Future<void> sendNotificationUsingApi({
+    required String? token,
+    required String? title,
+    required String? body,
+    required Map<String, dynamic>? data,
+  }) async {
+    String serverKey = await GetServerKey().getServerKeyToken();
+    print("notification server key => ${serverKey}");
+    String url =
+        "https://fcm.googleapis.com/v1/projects/easyshopping-ce06a/messages:send";
+
+    var headers = <String, String>{
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $serverKey',
+    };
+
+    //mesaage
+    Map<String, dynamic> message = {
+      "message": {
+        "token": token,
+        "notification": {"body": body, "title": title},
+        "data": data,
+      }
+    };
+
+    //hit api
+    final http.Response response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: jsonEncode(message),
+    );
+
+    if (response.statusCode == 200) {
+      print("Notification Send Successfully!");
+    } else {
+      print("Notification not send!");
+    }
+  }
+}
